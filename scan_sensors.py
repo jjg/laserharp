df2b775acc96b8675dd1d8f6398e3b30fd4ac271
Jason J. Gullickson <mr@jasongullickson.com>
2021-02-13 15:50:53 -0600

    Basic note selection working.

--- scan_sensors.py
+++ scan_sensors.py
@@ -1,10 +1,9 @@
 import time
 import ssd1306
 
-from machine import Pin, I2C
+from machine import Pin, I2C, Timer
 
 # Init display
-
 oled_width = 128
 oled_height = 64
 
@@ -17,27 +16,31 @@ sensors = [
         "GPIO": 36,     # TODO: Replace if needed once the docs load...
     },
 ]
-    
-# Enumerate sensor pins
-for sensor in sensors:
-    # Select sensor 
-    print("sensor note: " + sensor['note'])
-    print("sensor_pin: " + str(sensor['GPIO']))
-    sensor_pin = Pin(sensor["GPIO"], Pin.IN) # Enable pull-up?  Pin.PULL_UP
-    
-    # Read sensor
-    sensor_value = sensor_pin.value()
-    print("sensor_value: " + str(sensor_value))
-    
-    if sensor_value:
-        # TODO: Print status on oled
-        print("note " + sensor['note'] + " is high")
-        oled.text("play " + sensor['note'], 0, 0)
-        oled.show()
-    else:
-        # TODO: Find another way to erase the display
-        oled.text("", 0, 0)
+
+
+def scan(self):
+    # Enumerate sensor pins
+    for sensor in sensors:
+        # Select sensor 
+        print("sensor note: " + sensor['note'])
+        print("sensor_pin: " + str(sensor['GPIO']))
+        sensor_pin = Pin(sensor["GPIO"], Pin.IN) # Enable pull-up?  Pin.PULL_UP
+        
+        # Read sensor
+        sensor_value = sensor_pin.value()
+        print("sensor_value: " + str(sensor_value))
+        
+        if sensor_value:
+            oled.fill(0)
+            
+        else:
+            # Print status on oled
+            print("note " + sensor['note'] + " is high")
+            oled.text("play " + sensor['note'], 0, 0)
+            
+            # TODO: Play the note
+            
         oled.show()
 
-    # Wait 1 sec.
-    time.sleep(1)
+tim1 = Timer(0)
+tim1.init(period=100, mode=Timer.PERIODIC, callback=scan)
